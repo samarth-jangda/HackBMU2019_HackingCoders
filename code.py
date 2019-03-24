b9818388292e371a48514ac7e1a3453b068ea9fe
Samarth Jangda <48179563+samarth-jangda@users.noreply.github.com>
2019-03-24 07:47:14 +0530

    Update code.py

--- code.py
+++ code.py
@@ -68,4 +68,24 @@ plt.clf()          # clf is clear data frame
 # Preparing bars for Average DR
 barDR = plt.bar(causes["Year"], causes["Inst. Deaths"], width = 0.65, align = 'center', color = 'magenta',
                 label = 'Inst. Deaths')                                      #This will plot bar graph
+# Setting graph properties
+# plt.yticks(range(0, 6000, 250))
+# plt.xticks(range(len(deaths["Diseases"])), deaths["Diseases"])
+plt.ylabel("Instant Deaths")               # The labling of y axis will be here
+plt.xlabel("Year")                   # The labeling of x axis will be here
+plt.title("Past 10 year data of deaths due to heart attack in Delhi")   #  This will give the title to the plot
+plt.legend(fontsize="small")
+plt.grid()                           # This gives the grid in the graph
+# for (rect, label) in zip(barDR, yrly_avg_DR[-1::-1]):
+#     plt.text(rect.get_x() + rect.get_width() / 2, rect.get_height() + 150, format(label, '.2f'), ha='center', va='top')
+# In order to show the whole graph use:
+plt.show()
+# Then print the deaths
+print(causes)
+death_rate = pd.read_csv("C:\\Users\\LENOVO\\Documents\\Book3.csv")
+plt.clf()          # clf is clear data frame
+# plt.figure(figsize = (18, 14), dpi=120)
 
+# Preparing bars for Average DR
+barDR = plt.bar(diseases["Country"], diseases["Heart"], width = 0.45, align = 'center', color = 'blue',
+                label = 'Diseases')
