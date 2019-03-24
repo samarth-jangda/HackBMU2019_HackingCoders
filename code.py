418f21a06a55f1c0b5fec806baf689bfbfb150f8
Samarth Jangda <48179563+samarth-jangda@users.noreply.github.com>
2019-03-24 09:04:14 +0530

    Update code.py

--- code.py
+++ code.py
@@ -39,6 +39,7 @@ print(4)
 print(diseases)
 
 plt.clf()          # clf is clear data frame
+
 # plt.figure(figsize = (18, 14), dpi=120)
 
 # Preparing bars for Average DR
@@ -89,3 +90,17 @@ plt.clf()          # clf is clear data frame
 # Preparing bars for Average DR
 barDR = plt.bar(diseases["Country"], diseases["Heart"], width = 0.45, align = 'center', color = 'blue',
                 label = 'Diseases')
+# Setting graph properties
+# plt.yticks(range(0, 6000, 250))
+# plt.xticks(range(len(deaths["Diseases"])), deaths["Diseases"])
+plt.ylabel("Heart diseases")               # The labling of y axis will be here
+plt.xlabel("Country")                   # The labeling of x axis will be here
+plt.title("Deaths in every country due to heart Disease")   #  This will give the title to the plot
+plt.legend(fontsize='small')
+plt.grid()                           # This gives the grid in the graph
+# for (rect, label) in zip(barDR, yrly_avg_DR[-1::-1]):
+#     plt.text(rect.get_x() + rect.get_width() / 2, rect.get_height() + 150, format(label, '.2f'), ha='center', va='top')
+# In order to show the whole graph use:
+plt.show()
+# Then print the deaths
+print(diseases)
