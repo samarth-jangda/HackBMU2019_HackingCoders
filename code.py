e58a1d9d1c3df1954456066b14027cd2ca1f7001
Samarth Jangda <48179563+samarth-jangda@users.noreply.github.com>
2019-03-24 09:50:51 +0530

    Update code.py

--- code.py
+++ code.py
@@ -104,3 +104,28 @@ plt.grid()                           # This gives the grid in the graph
 plt.show()
 # Then print the deaths
 print(diseases)
+#This is dataset 3
+causes = pd.read_csv("C:\\Users\\LENOVO\\Documents\\Book1.csv")
+print(causes)
+#After that we will be plotting the graph
+plt.clf()          # clf is clear data frame
+# plt.figure(figsize = (18, 14), dpi=120)
+
+# Preparing bars for Average DR
+barDR = plt.bar(death_rate["Age grp."], death_rate["Male"], width = 0.65, align = 'center', color = 'green',
+                label = 'Age groups')                                      #This will plot bar graph
+
+# Setting graph properties
+# plt.yticks(range(0, 6000, 250))
+# plt.xticks(range(len(deaths["Diseases"])), deaths["Diseases"])
+plt.ylabel("Male")               # The labling of y axis will be here
+plt.xlabel("Age groups")                   # The labeling of x axis will be here
+plt.title("Age wise deaths due to heart attack in Delhi")   #  This will give the title to the plot
+plt.legend(fontsize="small")
+plt.grid()                           # This gives the grid in the graph
+# for (rect, label) in zip(barDR, yrly_avg_DR[-1::-1]):
+#     plt.text(rect.get_x() + rect.get_width() / 2, rect.get_height() + 150, format(label, '.2f'), ha='center', va='top')
+# In order to show the whole graph use:
+plt.show()
+# Then print the deaths
+print(death_rate)
